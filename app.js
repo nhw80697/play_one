ad3de6b97a5ebce6f820e821b578a8242248b92b
naf <nhw80697@gmail.com>
2022-02-07 15:27:28 +0200

    add code

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const square = ` <div class="square" ></div><div class="square" ></div><div class="square" ></div><div class="square" ></div><div class="square" ></div>`;
-var message = document.getElementById("message");
-
 function addBoard(){
     document.body.innerHTML += `<div class="main_board" id="main_board"></div>`;
     const main_board = document.getElementById("main_board").innerHTML += square;
 }
+var string = "";
+addBoard()
+var squares = document.getElementById("main_board").childNodes;
 
-document.body.addEventListener('onkeydown',(e)=>{console.log(e.key); message.innerHTML += e.key})
-// document.addEventListener("keydown",e =>  message.innerText += e.key)
+function addKey(e){
+    
+    if (!isNaN(e.key) && string.length <= 5){
+        string += e.key;
+        squares[string.length].innerHTML = e.key;
+    }
+
+    if (e.keyCode === 13 && string.length == 5){
+        addBoard()
+    }
+}
+
+
