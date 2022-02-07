4dc076d8d2d0f56151d26e20e6ae3362a5d6523f
office <nhw80697@gmail.com>
2022-02-07 22:44:12 +0200

    change

--- app.js
+++ app.js
@@ -1,22 +1,47 @@
-const square = ` <div class="square" ></div><div class="square" ></div><div class="square" ></div><div class="square" ></div><div class="square" ></div>`;
 function addBoard(){
-    document.body.innerHTML += `<div class="main_board" id="main_board"></div>`;
-    const main_board = document.getElementById("main_board").innerHTML += square;
-}
-var string = "";
-addBoard()
-var squares = document.getElementById("main_board").childNodes;
+    var mainBoardTemplet = `<div class="main_board"></div>`;
+    var squareTemplet = ` <div class="square" ></div><div class="square" ></div><div class="square" ></div><div class="square" ></div><div class="square" ></div>`;
 
-function addKey(e){
-    
-    if (!isNaN(e.key) && string.length <= 5){
+    document.body.innerHTML += mainBoardTemplet;
+    const main_board = document.getElementsByClassName("main_board");
+    main_board[main_board.length-1].innerHTML += squareTemplet;
+}
+
+var random = randomNumber();
+var string = "";
+function addKey(e){    
+    let main_board = document.getElementsByClassName("main_board");
+    let squares = main_board[main_board.length-1].childNodes;
+
+    if (!isNaN(e.key) && string.length < 5){
         string += e.key;
         squares[string.length].innerHTML = e.key;
     }
 
     if (e.keyCode === 13 && string.length == 5){
+        console.log(random);
+        [...random].forEach((value, key) => {
+            [...string].forEach((valueString, keyString)=>{
+                console.log(value + "-" + valueString)
+                if (value == valueString && key == keyString){
+                    console.log(squares[key + 1]);
+                    squares[key + 1].style = "background-color: red";
+                }else if(value == valueString){
+                    squares[keyString + 1].style = "background-color: yellow";
+                }
+            })
+        });
+        string = "";
         addBoard()
     }
 }
 
+function randomNumber(){
+    let num = Math.random() * 100000;
+    while (num < 10000){num = Math.random() * 100000;}
+    num = Math.round(num).toString();
+    return num;
+}
+
+addBoard();
 
